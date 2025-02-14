915bf7494a690c515bf36704ac09e14afa8aa159
miguel <miguel@computebnb.io>
2025-02-14 14:19:38 +0100

    S

--- modules/llm_local/setup.sh
+++ modules/llm_local/setup.sh
@@ -13,22 +13,6 @@ if ! command -v nvcc &> /dev/null; then
     exit 1
 fi
 
-MODEL_FILE="qwen2.5-coder-14b-instruct-q4_k_m.gguf"
-if [ ! -f "$MODEL_FILE" ]; then
-    echo "Downloading model file from Hugging Face..."
-    wget https://huggingface.co/Qwen/Qwen2.5-Coder-14B-Instruct-GGUF/resolve/main/$MODEL_FILE
-else
-    echo "Model file '$MODEL_FILE' already exists, skipping download."
-fi
-
-echo "Setting environment variables for CUDA and CMake..."
-
-export FORCE_CMAKE=1
-
-echo "Installing llama-cpp-python..."
-export CMAKE_ARGS="-DLLAMA_CUBLAS=on" 
-pip install llama-cpp-python --extra-index-url https://abetlen.github.io/llama-cpp-python/whl/cu125
-
 echo "Installing project dependencies from local_llm_requirements.txt..."
 pip install -r autoppia_iwa_module/modules/llm_local/requirements.txt
 
