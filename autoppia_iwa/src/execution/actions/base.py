13102a62b6cfe85ee135905127a7c69d8b2ade74
DaryxXx <mauricio.munozlopez@gmail.com>
2025-02-15 22:01:53 +0100

    pydantic fixes

--- autoppia_iwa/src/execution/actions/base.py
+++ autoppia_iwa/src/execution/actions/base.py
@@ -105,11 +105,7 @@ class BaseAction(BaseModel, IAction):
 
     type: str = Field(discriminator=True)  # Esto es más limpio que usar model_config
 
-    model_config = ConfigDict(from_attributes=True)
-
-    class Config:
-        # Allow unknown fields so we don't lose them.
-        extra = "allow"
+    model_config = ConfigDict(from_attributes=True, extra="allow")
 
     def __init__(self, **data):
         if 'type' not in data:
