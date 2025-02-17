2d9614f9e573541a4e57312d6c7563dc1cde21cc
miguel <miguel@computebnb.io>
2025-02-17 16:17:16 +0100

    updated torch version

--- modules/llm_local/setup.sh
+++ modules/llm_local/setup.sh
@@ -7,17 +7,11 @@ sudo apt-get install -y python3 python3-pip python3-dev python3.10 python3.10-ve
 python3.10 -m venv llm_env && source llm_env/bin/activate
 echo "Checking CUDA installation..."
 
-# # Check if CUDA is installed
-# if ! command -v nvcc &> /dev/null; then
-#     echo "❌ CUDA Toolkit not found."
-#     exit 1
-# fi
-
 echo "Installing project dependencies from local_llm_requirements.txt..."
 pip install -r autoppia_iwa_module/modules/llm_local/requirements.txt
 
 # NOTE - VERSION 12.6
-pip3 install torch==2.4.1 #torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126
+pip3 install torch==2.4.1 
 
 echo "Installing and configuring PM2 service..."
 sudo apt install -y npm
