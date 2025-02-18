0d9f87874fcec953a0d5171810838a6ec48d8188
Gul Shair <Gul.shair@rev9solutions.com>
2025-02-18 11:07:31 +0500

    reduced timeout to 2.5 seconds

--- autoppia_iwa/src/evaluation/evaluator/evaluator.py
+++ autoppia_iwa/src/evaluation/evaluator/evaluator.py
@@ -30,7 +30,7 @@ class EvaluatorConfig(BaseModel):
     save_results_in_db: bool = False
     task_delay_in_seconds: float = Field(default=0.2, gt=0)
     chunk_size: int = Field(default=3, gt=0)
-    browser_timeout: float = Field(default=10000, gt=0)
+    browser_timeout: float = Field(default=2500, gt=0) #TODO: Reduce to 2.5 second to need to increase in future
     event_monitor_interval: float = Field(default=0.1, gt=0, le=0.5)
 
 
