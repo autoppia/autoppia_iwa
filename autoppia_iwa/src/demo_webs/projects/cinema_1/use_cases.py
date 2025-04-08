d248f616ebe7a367ecb9280dca66c314e8a3256c
Gul Shair <Gul.shair@rev9solutions.com>
2025-04-08 13:13:05 +0500

    add film use case tested

--- autoppia_iwa/src/demo_webs/projects/cinema_1/use_cases.py
+++ autoppia_iwa/src/demo_webs/projects/cinema_1/use_cases.py
@@ -1109,8 +1109,8 @@ ALL_USE_CASES = [
     # ====> SOLVE THESE <====
     ############################
     # EDIT_FILM_USE_CASE, # Solved
-    # FILM_DETAIL_USE_CASE,
-    ADD_FILM_USE_CASE,
+    FILM_DETAIL_USE_CASE,
+    # ADD_FILM_USE_CASE, # Solved
     #############################
     # ====> SOLVED RIVER <====
     #############################
