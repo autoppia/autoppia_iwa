10d62b0af98602d0672df2dbf14d22a4235944cb
Gul Shair <Gul.shair@rev9solutions.com>
2025-04-09 15:49:05 +0500

    typo

--- autoppia_iwa/src/evaluation/evaluator/evaluator.py
+++ autoppia_iwa/src/evaluation/evaluator/evaluator.py
@@ -360,7 +360,7 @@ class ConcurrentEvaluator(IEvaluator):
         async with async_playwright() as playwright:
             browser, context = None, None
             try:
-                browser = await playwright.chromium.launch(headless=EVALUATOR_HEADLESS, slowmo=2000)
+                browser = await playwright.chromium.launch(headless=EVALUATOR_HEADLESS, slow_mo=2000)
                 context = await browser.new_context(extra_http_headers={"X-WebAgent-Id": web_agent_id})
                 context.set_default_timeout(self.config.browser_timeout)
                 page = await context.new_page()
