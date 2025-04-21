fb6ec2e0786797d5cf36aef834dc0ff1c1da8f71
Gul Shair <Gul.shair@rev9solutions.com>
2025-04-21 13:00:23 +0500

    relevant data updated

--- autoppia_iwa/src/demo_webs/projects/books_2/relevant_data.py
+++ autoppia_iwa/src/demo_webs/projects/books_2/relevant_data.py
@@ -1,4 +1,4 @@
 # ================ Relevant Data ================
 RELEVANT_DATA = {
-    "user_for_login": {"username": "user<web_agent_id>", "password": "password123"},
+    "user_for_login": {"username": "user<web_agent_id>", "password": "PASSWORD"},
 }
