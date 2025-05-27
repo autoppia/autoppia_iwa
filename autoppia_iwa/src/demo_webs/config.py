38849900e915b4e46f553ff2e938f04aeb79e54b
Gul Shair <Gul.shair@rev9solutions.com>
2025-05-27 12:57:27 +0500

    updated benchmark to accept a single parameter to run projects with 1-based selections and also optional plot and cache (task or task solution) saving

--- autoppia_iwa/src/demo_webs/config.py
+++ autoppia_iwa/src/demo_webs/config.py
@@ -1,12 +1,18 @@
 import sys
 from pathlib import Path
 
+sys.path.append(str(Path(__file__).resolve().parents[3]))
+
 from .classes import WebProject
 from .projects.books_2.main import books_project
 from .projects.cinema_1.main import cinema_project
-from .projects.dining_4.main import dining_project
-from .projects.omnizone_3.main import omnizone_project
 
-sys.path.append(str(Path(__file__).resolve().parents[3]))
+# from .projects.dining_4.main import dining_project
+# from .projects.omnizone_3.main import omnizone_project
 
-demo_web_projects: list[WebProject] = [cinema_project, books_project, omnizone_project, dining_project]
+demo_web_projects: list[WebProject] = [
+    cinema_project,
+    books_project,
+    # omnizone_project,
+    # dining_project,
+]
