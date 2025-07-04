ffed188ebf43d4889d2ed3f4eebbb7c84d4cf65d
Gul Shair <Gul.shair@rev9solutions.com>
2025-07-04 11:52:15 +0500

    draft main file added

---
+++ autoppia_iwa/src/demo_webs/projects/autocrm_5/main.py
@@ -0,0 +1,17 @@
+from autoppia_iwa.src.demo_webs.classes import WebProject
+
+from ...utils import get_backend_url, get_frontend_url
+from .events import EVENTS
+
+# from .use_cases import ALL_USE_CASES
+
+FRONTEND_PORT_INDEX = 4
+BACKEND_PORT_INDEX = 4
+dining_project = WebProject(
+    id="crm",
+    name="Autoppia CRM",
+    frontend_url=get_frontend_url(index=FRONTEND_PORT_INDEX),
+    backend_url=get_backend_url(index=BACKEND_PORT_INDEX, symmetric=True),
+    events=EVENTS,
+    # use_cases=ALL_USE_CASES,
+)
