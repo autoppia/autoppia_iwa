1ef00c85d0416121a940e5ebc61b000bce7257cf
Gul Shair <Gul.shair@rev9solutions.com>
2025-07-31 16:31:24 +0500

    update

--- autoppia_iwa/entrypoints/benchmark.py
+++ autoppia_iwa/entrypoints/benchmark.py
@@ -53,10 +53,12 @@ PROJECTS_TO_RUN: list[WebProject] = [
     # demo_web_projects[3],
     # demo_web_projects[4],
     demo_web_projects[5],
+    # demo_web_projects[6], # Not available now (DON'T USE)
+    # demo_web_projects[7],
 ]
 AGENTS: list[IWebAgent] = [
-    # ApifiedWebAgent(id="3", name="AutoppiaAgent", host="127.0.0.1", port=7000, timeout=120),
-    ApifiedWebAgent(id="2", name="AutoppiaAgent2", host="127.0.0.1", port=5000, timeout=120),
+    ApifiedWebAgent(id="2", name="AutoppiaAgent1", host="127.0.0.1", port=5000, timeout=120),
+    # ApifiedWebAgent(id="3", name="AutoppiaAgent2", host="127.0.0.1", port=7000, timeout=120),
 ]
 
 config = BenchmarkConfig(projects_to_run=PROJECTS_TO_RUN, agents=AGENTS)
