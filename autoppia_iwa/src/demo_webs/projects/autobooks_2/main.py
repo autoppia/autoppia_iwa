e7140458c9d53b5937a3a56bcf4fb2bd2a775e16
Riiveer <mazziitoozz@gmail.com>
2025-12-08 20:14:06 +0100

    update

--- autoppia_iwa/src/demo_webs/projects/autobooks_2/main.py
+++ autoppia_iwa/src/demo_webs/projects/autobooks_2/main.py
@@ -1,16 +1,18 @@
 from autoppia_iwa.src.demo_webs.classes import WebProject
 
-from ...utils import get_backend_service_url, get_frontend_url
+from ...utils import get_backend_service_url, get_frontend_url, get_web_version
 from .events import EVENTS
 from .relevant_data import RELEVANT_DATA
 from .use_cases import ALL_USE_CASES
 
 FRONTEND_PORT_INDEX = 1
+_frontend_url = get_frontend_url(index=FRONTEND_PORT_INDEX)
 books_project = WebProject(
     id="autobooks",
     name="Autoppia Books",
-    frontend_url=get_frontend_url(index=FRONTEND_PORT_INDEX),
+    frontend_url=_frontend_url,
     backend_url=get_backend_service_url(),
+    version=get_web_version("autobooks", frontend_url=_frontend_url),
     events=EVENTS,
     relevant_data=RELEVANT_DATA,
     use_cases=ALL_USE_CASES,
--- autoppia_iwa/src/demo_webs/projects/autocrm_5/main.py
+++ autoppia_iwa/src/demo_webs/projects/autocrm_5/main.py
@@ -1,15 +1,17 @@
 from autoppia_iwa.src.demo_webs.classes import WebProject
 
-from ...utils import get_backend_service_url, get_frontend_url
+from ...utils import get_backend_service_url, get_frontend_url, get_web_version
 from .events import EVENTS
 from .use_cases import ALL_USE_CASES
 
 FRONTEND_PORT_INDEX = 4
+_frontend_url = get_frontend_url(index=FRONTEND_PORT_INDEX)
 crm_project = WebProject(
     id="autocrm",
     name="Autoppia CRM",
-    frontend_url=get_frontend_url(index=FRONTEND_PORT_INDEX),
+    frontend_url=_frontend_url,
     backend_url=get_backend_service_url(),
+    version=get_web_version("autocrm", frontend_url=_frontend_url),
     events=EVENTS,
     use_cases=ALL_USE_CASES,
 )
