2c80125d20f6149ceac7c156c05cd7ff57000054
rodriggod <rodriggod@users.noreply.github.com>
2026-02-27 13:53:25 +0100

    fix(sonar): mark component props as read-only in ClientBody

--- modules/create_web_project/template/projects/autodining/frontend/src/app/ClientBody.tsx
+++ modules/create_web_project/template/projects/autodining/frontend/src/app/ClientBody.tsx
@@ -4,9 +4,9 @@ import { useEffect } from "react";
 
 export default function ClientBody({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   // Remove any extension-added classes during hydration
   useEffect(() => {
     // This runs only on the client after hydration
